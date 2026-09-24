7190c714816e007f5cc746cfee4c4b64d3e38a8d
Maxim Tarasov <40745@ma-web.nl>
2026-09-24 15:23:09 +0200

    1

---
+++ destinations.sql
@@ -0,0 +1,8 @@
+INSERT INTO `destinations` (`id`, `city`, `country`, `image`, `number_of_bookings`)
+VALUES
+	(1, 'Amsterdam', 'Nederland', 'amsterdam.jpg', 812),
+	(2, 'Parijs', 'Frankrijk', 'parijs.jpg', 1190),
+	(3, 'New York', 'Verenigde Staten', 'new-york.jpg', 921),
+	(4, 'Kaapstad', 'Zuid-Afrika', 'kaapstad.jpg', 230),
+	(5, 'Sydney', 'Australië', 'sydney.jpg', 534),
+	(6, 'Rio de Janeiro', 'Brazilië', 'rio-de-janeiro.jpg', 497);
